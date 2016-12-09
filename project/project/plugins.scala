c4414a54f81e04efddb63747bcf1124daeb4105c
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-12-10 04:22:11 +0900

    Modify Scala version

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object PluginDef extends Build {
   lazy val root = Project("plugins", file(".")).dependsOn(g8plugin)
-  lazy val g8plugin = ProjectRef(uri("git://github.com/n8han/giter8#0.6.8"), "giter8-plugin")
+  lazy val g8plugin = ProjectRef(uri("git://github.com/foundweekends/giter8#v0.7.1"), "giter8-plugin")
 }
